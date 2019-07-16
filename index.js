d1b8d2d1cd6809cacb6d6b32603e0b35e1ac5c72
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-07-16 15:17:08 +0200

    boilerplate setup

--- index.js
+++ index.js
@@ -6,19 +6,29 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const db = require("./config").mongoURI;
+
+//router imports
+const boilerplateMidd = require('./routes/boilerplate.routes');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// connect to mongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+//routers
+app.use('/api/boilerplate', boilerplateMidd)
+
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
-mongoose
-  .connect("mongodb://localhost:27017/test", { useNewUrlParser: true })
-  .then(
-    app.listen(port, () =>
-      console.log(`Example app listening on port ${port}!`)
-    ),
-    err => {
-      console.log(err);
-    }
-  );
+app.listen(port, () => console.log(`server running on port ${port}`));
---
+++ routes/boilerplate.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+//model
+const Item = require('../models/boilerplate.model');
+
+//routes
+router.get('/', (req,res)=>{
+    //Item.find()
+})
+
+module.exports = router;
