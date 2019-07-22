c3c895c742043a5cd0279dc8d8c7677fccbe5342
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-07-22 08:42:17 +0200

    token validation custom middleware

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const db = require("./config").mongoURI;
-
-const jwt = require('jsonwebtoken');
-const azureJWT = require('azure-jwt-verify');
-
+const validateToken = require("./utils").validateToken;
 //router imports
-const boilerplateMidd = require('./routes/boilerplate.routes');
+const boilerplateMidd = require("./routes/boilerplate.routes");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(validateToken);
 
 //connect to mongoDB
+const db = require("./config").mongoURI;
 mongoose
-  .connect(db, {useNewUrlParser: true})
+  .connect(db, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB Connected");
   })
@@ -29,26 +27,10 @@ mongoose
   });
 
 //routers
-app.use('/api/boilerplate', boilerplateMidd)
+app.use("/api/boilerplate", boilerplateMidd);
+
+app.get("/hello", (req, res) => res.send("Hello World!"));
 
 
-app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/jwt", (req,res)=>{
-  let jwtToken = req.body.token;
-  let tokenData = jwt.decode(jwtToken) // You can find this url in Azure Active Directory B2C Section
-  const config = {
-      JWK_URI: "https://login.microsoftonline.com/1b59fec5-1d4e-4835-8dc9-41f3e5a6ae9d/discovery/keys?appid=7c60b2af-e970-4c05-8278-67564929e213",
-      ISS: tokenData.iss,
-      AUD: tokenData.aud
-  };
-  azureJWT.verify(jwtToken, config).then(function(decoded){
-    console.log(decoded)
-    
-    }, function(error){
-      console.log(error)
-    
-    })
-});
-//https://login.microsoftonline.com/{tenant}/.well-known/openid-configuration?appid={appID}
 app.listen(port, () => console.log(`server running on port ${port}`));
---
+++ utils.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const azureJWT = require("azure-jwt-verify");
+
+const tenant = require("./config").tenant;
+const appId = require("./config").appId;
+
+module.exports = {
+  validateToken: (req, res, next) => {
+    const authorizationHeader = req.headers.authorization;
+    let result;
+    if (authorizationHeader) {
+      const token = req.headers.authorization.split(' ')[1];
+      try {
+        // verify makes sure that the token hasn't expired and has been issued by us
+        let tokenData = jwt.decode(token); // You can find this url in Azure Active Directory B2C Section
+        const config = {
+          JWK_URI:
+            `https://login.microsoftonline.com/${tenant}/discovery/keys?appid=${appId}`,
+          ISS: tokenData.iss,
+          AUD: tokenData.aud
+        };
+        azureJWT.verify(token, config).then(
+          function(decoded) {
+            //res.json(decoded);
+            next();
+          },
+          function(error) {
+            res.json(error);
+          }
+        );
+        
+      } catch (err) {
+        result = { 
+            error: `Authentication error. Token invalid.`,
+            status: 401
+          };
+          res.status(401).send(result);
+      }
+    } else {
+      result = { 
+        error: `Authentication error. Token required.`,
+        status: 401
+      };
+      res.status(401).send(result);
+    }
+  }
+};
