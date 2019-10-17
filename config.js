6fc1d591b517f45585f7286c90541f0cf7b6467c
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-10-17 22:07:02 +0200

    depl

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/test",
+    mongoURI: "mongodb://hanad:MaJsToR97@ds137008.mlab.com:37008/ibu-portal",//"mongodb://localhost:27017/test",
     tenant: "1b59fec5-1d4e-4835-8dc9-41f3e5a6ae9d",
     appId: "7c60b2af-e970-4c05-8278-67564929e213"
 }
